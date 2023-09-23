96e15f6bb76db550068a1f128d69a332e647b55b
UrvashiBhadaniya <145823903+UrvashiBhadaniya@users.noreply.github.com>
2023-09-23 10:12:46 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,56 @@
+                    #String
+s = "Urvashi"
+print(s)
+print(type(s))
+                    #integer
+a = 50
+print(a)
+print(type(a))
+                    #Float
+b = 17.5
+print(b)
+print(type(b))
+                    #list
+p = ["Veera","Nidhi","Bhoomi"]
+print(p)
+print(type(p))
+                    #Complex
+q = 1000j
+print(q)
+print(type(q))
+                    #Tuple
+c = ("Sahil","Ravi","Jayraj","Meet")
+print(c)
+print(type(c))
+                    #Set
+m = {"Urvashi","Dolly","Nidhi","Arti"}
+print(m)
+print(type(m))
+                    #Range
+n = range(1500)
+print(n)
+print(type(n))
+                    #dict
+o = {"Name":"Urvashi","age":"23"}
+print(o)
+print(type(o))
+                    #Frozenset
+r = frozenset({"Veera","Nidhi","Bhoomi"})
+print(r)
+print(type(r))
+                    #bool
+d = False
+print(d)
+print(type(d))
+                    #bytes
+f = b"Urvashi"
+print(f)
+print(type(f))
+                    #bytearray
+e = bytearray(10)
+print(e)
+print(type(e))
+                    #memoryview
+g = memoryview(bytes(15))
+print(g)
+print(type(g))
